894312799786c77b6a97e1199ad4f959da09e5fb
Suriya <suriyasandall@gmail.com>
2019-04-13 11:50:10 +1000

    NEW: Added basic draw funtions

--- Program.cs
+++ Program.cs
@@ -5,6 +5,22 @@ public class Program
 {
     public static void Main()
     {
+        Draw();
+    }
 
+    public static void Draw()
+    {
+        Window gameWindow;
+        Bitmap mapPng;
+
+        mapPng = new Bitmap("Snakes and Ladders Board","board800.png");
+        gameWindow = new Window("Game Window",1400,850);
+
+        gameWindow.Clear(Color.White);
+        gameWindow.DrawBitmap(mapPng,300,25);
+        gameWindow.Refresh();
+
+        SplashKit.Delay(5000);
     }
 }
+
